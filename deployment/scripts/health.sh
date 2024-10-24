a9ec1d8104e656dbc41ec3080e0c3311b34e6e38
Azumaxoid <azumax.android@gmail.com>
2024-10-24 17:13:10 +0900

    fix target ip

--- deployment/scripts/health.sh
+++ deployment/scripts/health.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+export GENY_IP_TARGET=PrivateIpAddress
 export GENY_IP=$(aws ec2 describe-instances --output=text --filters Name=instance-state-name,Values=running --query 'Reservations[].Instances[?Tags[?Key==`Name`].Value|[0]==`Genymotion`][].'$GENY_IP_TARGET)
 
 if [ "shell" = "$(timeout 3 ssh $GENY_IP -- whoami)" ];
